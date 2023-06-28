743d4d59c1924339d009fc6aedae415040533528
W159 <49504268+w159@users.noreply.github.com>
2023-06-28 15:08:41 -0400

    Added download location and updated filepath

--- Palo Alto - Global Protect Silent Install and Confgure.ps1
+++ Palo Alto - Global Protect Silent Install and Confgure.ps1
@@ -1,22 +1,35 @@
-﻿
-# Install latest Global Protect silently
-$GlobalProtectVersion = (Get-CimInstance -ClassName win32_product | Where-Object Name -Like "GlobalProtect").Version
-if ($GlobalProtectVersion -ne "6.2.0") {
-    Start-Process -FilePath ".\GlobalProtect64-6.2.0.msi" -ArgumentList '/quiet /L*V "C:\Utils\PAGlobalProtectInstaller.log"' -WindowStyle Hidden -Wait
+﻿# Create C:\Utils folder if it does not exist
+if (!(Test-Path -Path 'C:\Utils'))
+{
+    New-Item -Path 'C:\Utils' -ItemType Directory
 }
 
+# Download the latest Global Protect installer
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -UseBasicParsing 'https://onedrive.live.com/download?resid=af928892ebcfb343%21197755&authkey=!AGxsrzC_GLRHM4o' -OutFile 'C:\Utils\GlobalProtect64-6.2.0.msi'
+
+# Install latest Global Protect silently
+$GlobalProtectVersion = (Get-CimInstance -ClassName win32_product | Where-Object Name -Like 'GlobalProtect').Version
+if ($GlobalProtectVersion -ne '6.2.0')
+{
+    Start-Process -FilePath 'C:\Utils\GlobalProtect64-6.2.0.msi' -ArgumentList '/quiet /L*V "C:\Utils\PAGlobalProtectInstaller.log"' -WindowStyle Hidden -Wait
+}
 
 # Configure Global Protect Portals
 New-PSDrive -PSProvider 'Registry' -Name 'HKU' -Root 'HKEY_USERS'
 $users = Get-ChildItem 'HKU:\'
 $portalNames = @('vpn3.henssler.com', 'vpn2.henssler.com', 'vpn.henssler.com')
 
-foreach ($user in $users) {
+foreach ($user in $users)
+{
     $LocalUser = $user.PSChildName
 
-    foreach ($portalName in $portalNames) {
+    foreach ($portalName in $portalNames)
+    {
         $registryPath = "HKU:\$LocalUser\Software\Palo Alto Networks\GlobalProtect\Settings\$portalName"
-        if (!(Test-Path -Path $registryPath)) {
+        if (!(Test-Path -Path $registryPath))
+        {
             New-Item -Path $registryPath -Force
         }
     }
