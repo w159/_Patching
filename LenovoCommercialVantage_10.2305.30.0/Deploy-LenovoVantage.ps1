7122caf1db03819059fda61f6300b09922197fbc
W159 <49504268+w159@users.noreply.github.com>
2024-03-12 10:40:37 -0400

    Delete LenovoCommercialVantage_10.2305.30.0 directory
    Signed-off-by: W159 <49504268+w159@users.noreply.github.com>

--- LenovoCommercialVantage_10.2305.30.0/Deploy-LenovoVantage.ps1
+++
@@ -1,5 +0,0 @@
-$url = 'https://support.lenovo.com/us/en/solutions/hf003321-lenovo-vantage-for-enterprise'
-$response = Invoke-RestMethod -Uri $url
-$downloadLink = $response.Links | Where-Object { $_.href -match 'LenovoCommercialVantage.*\.zip' } | Select-Object -ExpandProperty href
-
-$downloadLink
--- LenovoCommercialVantage_10.2305.30.0/VantageService/Update-AddinComponents.ps1
+++
@@ -1,9 +0,0 @@
-# update the registry key 
-#  HKLM\SOFTWARE\LENOVO\VantageService  32bitview
-#  IsProloadInstallCompleted = "false"
-pushd $PSScriptRoot
-
-# update hypothesis and config servcie.
-&./VantageComponentUpdater.exe
-
-popd
