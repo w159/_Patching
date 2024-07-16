4a1adeb7c0623ca3481fcbf1f5addfaaf3067b6e
W159 <49504268+w159@users.noreply.github.com>
2024-07-16 09:23:32 -0400

    chore: Remove Palo Alto GlobalProtect software and related files
    - Uninstall Palo Alto GlobalProtect software using WMI
    - Remove GlobalProtect installer log file
    - Remove registry entries for Palo Alto Networks from all user profiles
    - Remove Palo Alto Networks folder from each user's AppData directory
    - Remove specified paths related to Palo Alto Networks

---
+++ Palo Alto - Global Protect Removal.ps1
@@ -0,0 +1,44 @@
+# Remove Palo Alto GlobalProtect software
+$PaloAltoGlobalProtect = Get-WmiObject -ClassName Win32_Product | Where-Object Name -Like 'GlobalProtect'
+if ($PaloAltoGlobalProtect) {
+     $PaloAltoGlobalProtect.Uninstall()
+}
+
+# Remove GlobalProtect installer log file
+$PAGlobalProtectLog = Test-Path 'C:\Utils\PAGlobalProtectInstaller.log'
+if ($PAGlobalProtectLog) {
+     Remove-Item -Path 'C:\Utils\PAGlobalProtectInstaller.log' -Force
+}
+
+# Remove registry entries for Palo Alto Networks from all user profiles
+New-PSDrive -PSProvider 'Registry' -Name 'HKU' -Root 'HKEY_USERS'
+$users = Get-ChildItem 'HKU:\'
+foreach ($user in $users) {
+     $LocalUser = $user.PSChildName
+     $registryPath = "HKU:\$LocalUser\Software\Palo Alto Networks"
+     if (Test-Path -Path $registryPath) {
+          Remove-Item -Path $registryPath -Force
+     }
+}
+
+# Remove Palo Alto Networks folder from each user's AppData directory
+$UserFolders = Get-ChildItem -Path 'C:\Users'
+foreach ($UserFolder in $UserFolders) {
+     $LocalUser = $UserFolder.Name
+     $GlobalProtectUserFolder = "C:\Users\$LocalUser\AppData\Local\Palo Alto Networks"
+     if (Test-Path -Path $GlobalProtectUserFolder) {
+          Remove-Item -Path $GlobalProtectUserFolder -Force
+     }
+}
+
+# Remove specified paths related to Palo Alto Networks
+$PathsToDelete = @(
+     'C:\Program Files\Palo Alto Networks',
+     'HKLM:\SOFTWARE\Palo Alto Networks'
+)
+
+foreach ($Path in $PathsToDelete) {
+     if (Test-Path -Path $Path) {
+          Remove-Item -Path $Path -Force -Recurse
+     }
+}
