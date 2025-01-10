9a20eb0608dce0be63648dd9ab3b088476745058
Jerry <da-jmorgan@henssler.com>
2025-01-10 11:14:46 -0500

    chore: Update Adobe CC Apps Updater script to change paths for Remote Update Manager

--- Adobe CC Apps Updater.ps1
+++ Adobe CC Apps Updater.ps1
@@ -11,25 +11,22 @@ Legacy Reader apps and those not part of Creative Cloud are not patched as part 
 $ProgressPreference = 'SilentlyContinue'
 
 # Check for the Adobe Remote Update Manager
-$AdobeRemoteUpdateManager_CHECK = Test-Path -Path 'C:\Utils\AdobeRUM\RemoteUpdateManager.exe'
+$AdobeRemoteUpdateManager_CHECK = Test-Path -Path 'C:\Windows\Utils\AdobeRUM\RemoteUpdateManager.exe'
 
-if ($AdobeRemoteUpdateManager_CHECK -eq $false)
-{
+if ($AdobeRemoteUpdateManager_CHECK -eq $false) {
     Write-Host 'Downloading Adobe Remote Update Manager'
     $UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
     Invoke-WebRequest -UseBasicParsing 'https://onedrive.live.com/download?cid=AF928892EBCFB343&resid=AF928892EBCFB343%21198740&authkey=APGjFRM5LgIBCHg' `
-        -OutFile 'C:\Utils\AdobeRemoteUpdateManager.zip' `
+        -OutFile 'C:\Windows\Utils\AdobeRemoteUpdateManager.zip' `
         -UserAgent $UserAgent
 
     New-Item C:\Utils -Name AdobeRUM -ItemType Directory -Force -ErrorAction SilentlyContinue
-    Expand-Archive -Path 'C:\Utils\AdobeRemoteUpdateManager.zip' -DestinationPath C:\Utils\AdobeRUM
-    Get-ChildItem -Path C:\Utils\AdobeRUM -Recurse | Unblock-File
+    Expand-Archive -Path 'C:\Windows\Utils\AdobeRemoteUpdateManager.zip' -DestinationPath C:\Windows\Utils\AdobeRUM
+    Get-ChildItem -Path C:\Windows\Utils\AdobeRUM -Recurse | Unblock-File
 
-    Start-Process -FilePath 'C:\Utils\AdobeRUM\RemoteUpdateManager.exe' -NoNewWindow -Wait
+    Start-Process -FilePath 'C:\Windows\Utils\AdobeRUM\RemoteUpdateManager.exe' -NoNewWindow -Wait
 
-}
-else
-{
+} else {
     Write-Host 'Adobe Remote Update Manager Found - Checking for updates'
-    Start-Process -FilePath 'C:\Utils\AdobeRUM\RemoteUpdateManager.exe' -NoNewWindow -Wait
+    Start-Process -FilePath 'C:\Windows\Utils\AdobeRUM\RemoteUpdateManager.exe' -NoNewWindow -Wait
 }
